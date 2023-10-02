c626522e192aeea12cc569b15657d1471534a200
Madhumitha <madhumithavenu03@gmail.com>
2023-10-02 17:49:42 +0530

    moving data to database

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ app.post('/submit', (req, res) => {
     const { name, email, mobile } = req.body;
 
     let sql_query = `INSERT INTO STUDENTS (NAME, EMAIL, MOBILE) VALUES('${name}', '${email}', '${mobile}');`;
+    
     connection.query(sql_query, (err, result) => {
         if (err) return console.log("Error while inserted data " + err);
         res.json({ 'message ': "insert was successfull", "result": result.insertId });
