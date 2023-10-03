503d5cde7d72ff1688fdef790fa8fc6fd458e36c
Madhumitha <madhumithavenu03@gmail.com>
2023-10-03 12:16:22 +0530

    Linking the seperated folder

--- routes/students-routes.js
+++ routes/students-routes.js
@@ -3,7 +3,6 @@ const express = require("express");
 const { homePage, insertValues } = require("../controllers/students-controller.js");
 
 const studentRouter = express.Router();
-
 studentRouter.get("/", homePage);
 studentRouter.post('/submit', insertValues);
 
